af894eb391635627fd79a74d69471df326694534
AoEiuV020 <aoeiuv020@gmail.com>
2024-05-09 01:19:55 +0800

    实现带时间时分秒的行的解析

--- packages/diary_split/lib/src/date_time_parser.dart
+++ packages/diary_split/lib/src/date_time_parser.dart
@@ -3,7 +3,10 @@
 import 'package:intl/intl.dart';
 
 class DateTimeParser {
-  final startTimeFormat = DateFormat("M月d日");
+  final startDateFormat = DateFormat("M月d日");
+  final lineTimeWithSecondsFormat = DateFormat("HH:mm:ss");
+  final lineTimeFormat = DateFormat("HH:mm");
+  DateTime current = DateTime.now();
   (DateTime?, String) parse(String line) {
     line = line.trim();
 
@@ -13,25 +16,44 @@ class DateTimeParser {
     // 根据第一个空格的位置拆分字符串
     String firstPart = line.substring(0, spaceIndex != -1 ? spaceIndex : null);
 
-    DateTime? date;
-    try {
-      date = parseStartTime(firstPart);
-    } catch (e) {}
-    if (date == null) {
-      return (null, line);
+    (DateTime?, String) result(DateTime date) {
+      // 能进入这个方法就说明date正常解析到位了，
+      current = date;
+      String secondPart =
+          spaceIndex != -1 ? line.substring(spaceIndex + 1).trim() : '';
+      return (date, secondPart);
     }
-    String secondPart =
-        spaceIndex != -1 ? line.substring(spaceIndex + 1).trim() : '';
-    return (date, secondPart);
+
+    try {
+      return result(parseLineWithSecondsTime(firstPart));
+    } catch (e) {}
+    try {
+      return result(parseLineTime(firstPart));
+    } catch (e) {}
+    try {
+      return result(parseStartDate(firstPart));
+    } catch (e) {}
+    // 到这里说明没解析到时间，
+    return (null, line);
   }
 
-  DateTime parseStartTime(String part) {
+  DateTime parseLineTime(String part) {
+    final date = lineTimeFormat.parse(part);
+    return current.copyWith(hour: date.hour, minute: date.minute, second: 0);
+  }
+
+  DateTime parseLineWithSecondsTime(String part) {
+    final date = lineTimeWithSecondsFormat.parse(part);
+    return current.copyWith(
+        hour: date.hour, minute: date.minute, second: date.second);
+  }
+
+  DateTime parseStartDate(String part) {
     // 将日期字符串转换为DateTime对象
-    var date = startTimeFormat.parse(part);
+    final date = startDateFormat.parse(part);
     // 获取当前年份
     int currentYear = DateTime.now().year;
     // 设置年份为当前年份
-    date = DateTime(currentYear, date.month, date.day);
-    return date;
+    return DateTime(currentYear, date.month, date.day);
   }
 }
--- packages/diary_split/test/diary_split_test.dart
+++ packages/diary_split/test/diary_split_test.dart
@@ -3,11 +3,21 @@ import 'package:test/test.dart';
 
 void main() {
   group('date format test', () {
+    final parser = DateTimeParser();
     test('start time parse', () {
-      final parser = DateTimeParser();
       final (date, text) = parser.parse('9月28日');
       expect(date.toString(), '${DateTime.now().year}-09-28 00:00:00.000');
       expect(text, '');
+    });
+    test('time text parse', () {
+      final (date, text) = parser.parse('18:31:32 打卡，下班，');
+      expect(date.toString(), '${DateTime.now().year}-09-28 18:31:32.000');
+      expect(text, '打卡，下班，');
+    });
+    test('time without seconds text parse', () {
+      final (date, text) = parser.parse('15:11 打个喷嚏，');
+      expect(date.toString(), '${DateTime.now().year}-09-28 15:11:00.000');
+      expect(text, '打个喷嚏，');
     });
   });
 }
